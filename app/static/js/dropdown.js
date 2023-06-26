4d745e3c51ed2532297d7d4571a2dbac3dda5d40
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2023-06-26 17:59:26 +0300

    dropdown animation was added

--- app/static/js/dropdown.js
+++ app/static/js/dropdown.js
@@ -1,22 +1,41 @@
 const dropdown = document.querySelectorAll(".dropdown-btn");
+const dropdownContents = document.querySelectorAll(".dropdown-container");
 
 dropdown.forEach(btn => {
   btn.addEventListener("click", () => {
-    btn.classList.toggle("bg-red-600");
     const dropdownContent = btn.nextElementSibling;
     const dropdownArrow = btn.lastElementChild;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.transition = 'all 0.3s'
-      dropdownArrow.style.transform = 'rotate(0)'
-      dropdownArrow.style.transition = 'all 0.3s'
-      dropdownContent.style.display = "none";
-      btn.classList.add("dark:hover:bg-gray-700");
-      btn.classList.add("hover:bg-gray-100");
-    } else {
-      dropdownContent.style.display = "block";
-      dropdownArrow.style.transform = 'rotate(90deg)'
+    const isClose = dropdownContent.classList.contains('dropdown-close');
+
+    btn.classList.toggle("bg-red-600");
+
+    dropdown.forEach(otherBtn => {
+      if (otherBtn !== btn) {
+        const otherContent = otherBtn.nextElementSibling;
+        const otherArrow = otherBtn.lastElementChild;
+
+        otherContent.classList.remove('dropdown-open');
+        otherContent.classList.add('dropdown-close');
+        otherBtn.classList.add("dark:hover:bg-gray-700");
+        otherBtn.classList.add("hover:bg-gray-100");
+        otherBtn.classList.remove("bg-red-600");
+        otherArrow.style.transform = 'rotate(0)';
+      }
+    });
+
+    if (isClose) {
+      dropdownContent.classList.remove('dropdown-close')
+      dropdownContent.classList.add('dropdown-open')
       btn.classList.remove("dark:hover:bg-gray-700");
       btn.classList.remove("hover:bg-gray-100");
-      }
+      dropdownArrow.style.transition = 'all 0.5s'
+      dropdownArrow.style.transform = 'rotate(90deg)'
+    } else {
+      dropdownContent.classList.remove('dropdown-open')
+      dropdownContent.classList.add('dropdown-close')
+      btn.classList.add("dark:hover:bg-gray-700");
+      btn.classList.add("hover:bg-gray-100");
+      dropdownArrow.style.transform = 'rotate(0)'
+    }
   })
 })
