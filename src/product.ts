68db1e7d165f69f8896f4c3635a03836bb762edf
Petro <xavrais312@gmail.com>
2023-07-28 08:06:12 +0300

    add csrf to ship/assign and hide assign

--- src/product.ts
+++ src/product.ts
@@ -684,7 +684,10 @@ function addShipAssignShareButton(
       <button assign-group-data=${group.replace(
         / /g,
         '_',
-      )} type="button" class="assign-product-button inline-flex items-center px-3 py-2.5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      )} type="button" id="assign-product-button-${group.replace(
+    / /g,
+    '_',
+  )}" class="assign-product-button inline-flex items-center px-3 py-2.5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
         Assign
       </button>
@@ -693,11 +696,15 @@ function addShipAssignShareButton(
   const shipProductBtn = shipAssignContainer.querySelector(
     `#ship-product-button-${group.replace(/ /g, '_')}`,
   );
+  const assignProductBtn = shipAssignContainer.querySelector(
+    `#assign-product-button-${group.replace(/ /g, '_')}`,
+  );
   if (
     productParam.available_quantity[group] === 0 ||
     !productParam.available_quantity[group]
   ) {
     shipProductBtn.classList.add('invisible');
+    assignProductBtn.classList.add('invisible');
   }
   const shareContainer = document.createElement('div');
   shareContainer.classList.add('sm:col-span-3');
