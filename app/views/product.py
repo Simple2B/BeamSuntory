1bf9a980658268d86955452ba9407f686451c2ab
Petro <xavrais312@gmail.com>
2023-08-14 17:54:02 +0300

    use product_groups on get all

--- app/views/product.py
+++ app/views/product.py
@@ -45,15 +45,19 @@ def get_all_products(request, query=None, count_query=None):
     master_groups_rows_obj = db.session.execute(m.MasterGroup.select()).all()
 
     groups_for_products_obj = db.session.execute(m.GroupProduct.select()).all()
-    groups_obj = db.session.execute(m.Group.select()).all()
     mastr_for_prods_groups_for_prods = {}
-    for group in groups_obj:
-        if group[0].master_groups.name not in mastr_for_prods_groups_for_prods:
-            mastr_for_prods_groups_for_prods[group[0].master_groups.name] = [group[0]]
+    for group in groups_for_products_obj:
+        if (
+            group[0].master_groups_for_product.name
+            not in mastr_for_prods_groups_for_prods
+        ):
+            mastr_for_prods_groups_for_prods[
+                group[0].master_groups_for_product.name
+            ] = [group[0]]
         else:
-            mastr_for_prods_groups_for_prods[group[0].master_groups.name].append(
-                group[0]
-            )
+            mastr_for_prods_groups_for_prods[
+                group[0].master_groups_for_product.name
+            ].append(group[0])
 
     # get all product_groups to list and compare in view.html
     product_groups_obj = db.session.execute(m.ProductGroup.select()).all()
