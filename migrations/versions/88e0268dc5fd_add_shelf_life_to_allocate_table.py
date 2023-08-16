dac9e29960d3bcad03d20b180e4dcd4f99188da6
Petro <xavrais312@gmail.com>
2023-08-16 14:42:21 +0300

    fix migration

--- migrations/versions/88e0268dc5fd_add_shelf_life_to_allocate_table.py
+++ migrations/versions/88e0268dc5fd_add_shelf_life_to_allocate_table.py
@@ -1,8 +1,8 @@
 """add shelf life to allocate table
 
-Revision ID: 03d9b3a41c8c
-Revises: bbaac3252c56
-Create Date: 2023-08-15 12:26:18.270006
+Revision ID: 88e0268dc5fd
+Revises: ab2e0f8ae3a8
+Create Date: 2023-08-16 14:41:39.430149
 
 """
 from alembic import op
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = "03d9b3a41c8c"
-down_revision = "bbaac3252c56"
+revision = "88e0268dc5fd"
+down_revision = "ab2e0f8ae3a8"
 branch_labels = None
 depends_on = None
 
@@ -21,6 +21,7 @@ def upgrade():
     with op.batch_alter_table("io_allocate_product", schema=None) as batch_op:
         batch_op.add_column(sa.Column("shelf_life_start", sa.DateTime(), nullable=True))
         batch_op.add_column(sa.Column("shelf_life_end", sa.DateTime(), nullable=True))
+
     # ### end Alembic commands ###
 
 
