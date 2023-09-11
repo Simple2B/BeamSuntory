8f53edb41be2724dbb5fd1d63c3c6d98923172cd
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2023-09-11 17:57:25 +0300

    added event_available_quantity route

--- app/forms/event.py
+++ app/forms/event.py
@@ -6,8 +6,9 @@ from wtforms.validators import DataRequired
 class EventFormCreate(FlaskForm):
     next_url = StringField("next_url")
     product_id = IntegerField("Product ID", [DataRequired()])
-    date_from = StringField("Date from", [DataRequired()])
-    date_to = StringField("Date to", [DataRequired()])
+    # date_from = StringField("Date from", [DataRequired()])
+    # date_to = StringField("Date to", [DataRequired()])
+    date_range = StringField("Date range", [DataRequired()])
     quantity = IntegerField("Quantity", [DataRequired()])
     cart_id = IntegerField("Cart ID", [DataRequired()])
     comment = StringField("Comment")
