c4ebdf17da29c711fa5628588fce4ced6a499aa8
Petro <xavrais312@gmail.com>
2023-09-14 11:38:57 +0300

    fix test

--- tests/test_products.py
+++ tests/test_products.py
@@ -110,8 +110,8 @@ def test_sort_product(mg_g_populate: FlaskClient):
         "/product/sort",
         data=dict(sort_by='{"Country": "Canada", "Brand": "JB"}'),
     )
-    assert ("populate_test_product" in response.text) is True
-    assert ("populate_test_prod2" in response.text) is True
+    assert ("322ewd3333rs" in response.text) is True
+    assert ("322ewd2222rs" in response.text) is True
     assert response.status_code == 200
 
     response = mg_g_populate.post(
@@ -119,17 +119,17 @@ def test_sort_product(mg_g_populate: FlaskClient):
         data=dict(sort_by='{"Brand": "JB"}'),
     )
     # TODO: fix test
-    # assert ("populate_test_product" in response.text) is False
-    assert ("populate_test_prod2" in response.text) is True
+    # assert ("322ewd3333rs" in response.text) is False
+    assert ("322ewd2222rs" in response.text) is True
     assert response.status_code == 200
 
     response = mg_g_populate.post(
         "/product/sort",
-        data=dict(sort_by={"Brand": "JB"}),
+        data=dict(sort='{"Brand": "JB"}'),
         follow_redirects=True,
     )
-    assert ("populate_test_product" in response.text) is True
-    assert ("populate_test_prod2" in response.text) is True
+    assert ("322ewd3333rs" in response.text) is True
+    assert ("322ewd2222rs" in response.text) is True
     assert ("Wrong sort" in response.text) is True
     assert response.status_code == 200
 
