502b1d4bde69d124a27baa59804daf3d9bc3ca82
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2023-09-18 19:44:25 +0300

    fix useless imports

--- app/models/ship_request.py
+++ app/models/ship_request.py
@@ -1,10 +1,4 @@
 from datetime import datetime
-from typing import TYPE_CHECKING
-import json
-
-if TYPE_CHECKING:
-    from .product import Product
-
 
 import sqlalchemy as sa
 from sqlalchemy import orm
@@ -14,9 +8,7 @@ from app import schema as s
 from .utils import ModelMixin
 
 from .cart import Cart
-from .warehouse import Warehouse
 from .store import Store
-from .warehouse_product import WarehouseProduct
 from .store_category import StoreCategory
 
 
@@ -64,36 +56,4 @@ class ShipRequest(db.Model, ModelMixin):
 
     @property
     def json(self):
-        # TODO refactor !
         return s.ShipRequest.model_validate(self).model_dump_json(by_alias=True)
-
-        mg_dict = json.loads(ujs)
-        mg_dict["current_order_carts"] = [
-            {
-                "id": cart.product.id,
-                "name": cart.product.name,
-                "SKU": cart.product.SKU,
-                "regular_price": cart.product.regular_price,
-                "retail_price": cart.product.retail_price,
-                "quantity": cart.quantity,
-                "image": cart.product.image,
-                "group": cart.group,
-                "warehouse": {"id": cart.warehouse_id, "name": cart.warehouse.name}
-                if cart.warehouse_id
-                else None,
-            }
-            for cart in db.session.scalars(
-                Cart.select().where(Cart.order_numb == mg_dict["order_numb"])
-            )
-        ]
-        mg_dict["warehouses"] = [
-            {"name": w.name, "id": w.id, "products_ids": []}
-            for w in db.session.execute(Warehouse.select()).scalars()
-        ]
-
-        for wh_prod in db.session.execute(WarehouseProduct.select()).scalars():
-            for warehouse in mg_dict["warehouses"]:
-                if warehouse["id"] == wh_prod.warehouse_id:
-                    warehouse["products_ids"].append(wh_prod.product_id)
-
-        return json.dumps(mg_dict)
