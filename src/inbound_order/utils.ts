780b0f6da3a402ed55a621554e6c5359d0b2d038
marynag <mgrabchak1109@gmail.com>
2023-09-20 18:33:26 +0300

    fix: inbound order: edit: datepicker

--- src/inbound_order/utils.ts
+++ src/inbound_order/utils.ts
@@ -1,10 +1,9 @@
 export const getDatepickerDateFormat = (dateString: string) => {
   const date = new Date(dateString);
-  const dateParts = date.toLocaleDateString().split('/');
 
-  const tmp = dateParts[0];
-  dateParts[0] = dateParts[1];
-  dateParts[1] = tmp;
-
-  return dateParts.join('/');
+  const month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+  const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+  const year = date.getFullYear();
+  
+  return month + '/' + day + '/' + year;
 }
