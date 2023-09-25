9e0d961cea91e6c434efea1f8eacf7a5021a6868
marynag <mgrabchak1109@gmail.com>
2023-09-25 17:44:48 +0300

    remove code duplicate

--- app/views/report_event.py
+++ app/views/report_event.py
@@ -114,57 +114,6 @@ def get_events_report():
     )
     return pagination, reports
 
-    filter_assign = s.FilterReportAssign.model_validate(dict(request.args))
-
-    query = m.Assign.select().order_by(m.Assign.id)
-    count_query = sa.select(sa.func.count()).select_from(m.Assign)
-
-    if filter_assign.q:
-        query = query.where(
-            m.Assign.product.has(m.Product.name.ilike(f"%{filter_assign.q}%"))
-            | m.Assign.product.has(m.Product.SKU.ilike(f"%{filter_assign.q}%"))
-            | m.Assign.user.has(m.User.username.ilike(f"%{filter_assign.q}%"))
-        )
-        count_query = count_query.where(
-            m.Assign.product.has(m.Product.name.ilike(f"%{filter_assign.q}%"))
-            | m.Assign.product.has(m.Product.SKU.ilike(f"%{filter_assign.q}%"))
-            | m.Assign.user.has(m.User.username.ilike(f"%{filter_assign.q}%"))
-        )
-
-    if filter_assign.username:
-        query = query.where(
-            m.Assign.user.has(m.User.username == filter_assign.username)
-        )
-
-    if filter_assign.master_group:
-        query = query.where(
-            m.Assign.product.has(
-                m.Product.product_groups.any(
-                    m.ProductGroup.parent.has(
-                        m.GroupProduct.name.ilike(f"%{filter_assign.master_group}%")
-                    )
-                )
-            )
-        )
-
-    if filter_assign.start_date:
-        print(filter_assign.start_date)
-        query = query.where(m.Assign.created_at >= filter_assign.start_date)
-
-    if filter_assign.end_date:
-        query = query.where(m.Assign.created_at <= filter_assign.end_date)
-
-    print(query)
-
-    pagination = create_pagination(total=db.session.scalar(count_query))
-
-    reports = db.session.scalars(
-        query.offset((pagination.page - 1) * pagination.per_page).limit(
-            pagination.per_page
-        )
-    )
-    return pagination, reports
-
 
 @report_blueprint.route("/event/api", methods=["GET"])
 @login_required
