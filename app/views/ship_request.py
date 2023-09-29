04d022406d1528e76df4f298898eb5cf624218cd
Petro <xavrais312@gmail.com>
2023-09-29 10:39:28 +0300

    report event for events only

--- app/views/ship_request.py
+++ app/views/ship_request.py
@@ -155,6 +155,10 @@ def create():
             )
             return redirect(url_for("product.get_all"))
 
+        # check if there are events in the ship request
+        # if yes - create event report
+        events_exist = False
+
         # Create ship request
         ship_request = m.ShipRequest(
             # NOTE: what status is default?
@@ -207,6 +211,7 @@ def create():
                     type=s.ReportEventType.created.value,
                     user=current_user,
                 )
+                events_exist = True
                 # creation event
                 event = m.Event(
                     date_reserve_from=start_date - timedelta(days=5),
@@ -232,6 +237,10 @@ def create():
 
         log(log.INFO, "Form submitted. Ship Request: [%s]", ship_request)
         flash("Ship request added!", "success")
+        if events_exist:
+            ship_request.report_event = report_event
+            report_event.save(False)
+
         ship_request.save(False)
 
         db.session.commit()
