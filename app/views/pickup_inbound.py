f81396cd203140cfc61f19c48c4cc7349e725077
marynag <mgrabchak1109@gmail.com>
2023-10-02 17:05:11 +0300

    add htmx to pickup_inbound

--- app/views/pickup_inbound.py
+++ app/views/pickup_inbound.py
@@ -22,49 +22,55 @@ pickup_inbound_blueprint = Blueprint(
 )
 
 
-@pickup_inbound_blueprint.route("/", methods=["GET"])
-@login_required
-def get_all():
-    form_create = f.InboundOrderCreateForm()
-    form_edit = f.InboundOrderUpdateForm()
-    form_sort: f.SortByStatusInboundOrderForm = f.SortByStatusInboundOrderForm()
+def get_pickup_inbound():
+    filter_pickup_inbound = s.FilterInboundOrder.model_validate(dict(request.args))
 
-    q = request.args.get("q", type=str, default=None)
     query = m.InboundOrder.select().order_by(desc(m.InboundOrder.id))
     count_query = sa.select(sa.func.count()).select_from(m.InboundOrder)
-    if q:
+
+    if filter_pickup_inbound.q:
         query = query.where(
-            m.InboundOrder.title.ilike(f"%{q}%")
-            | m.InboundOrder.order_id.ilike(f"%{q}%")
+            m.InboundOrder.title.ilike(f"%{filter_pickup_inbound.q}%")
+            | m.InboundOrder.order_id.ilike(f"%{filter_pickup_inbound.q}%")
         )
 
         count_query = count_query.where(
-            m.InboundOrder.title.ilike(f"%{q}%")
-            | m.InboundOrder.order_id.ilike(f"%{q}%")
+            m.InboundOrder.title.ilike(f"%{filter_pickup_inbound.q}%")
+            | m.InboundOrder.order_id.ilike(f"%{filter_pickup_inbound.q}%")
+        )
+
+    if filter_pickup_inbound.status:
+        query = query.where(
+            m.InboundOrder.status == s.InboundOrderStatus(filter_pickup_inbound.status)
+        )
+
+        count_query = count_query.where(
+            m.InboundOrder.status == s.InboundOrderStatus(filter_pickup_inbound.status)
         )
 
     pagination = create_pagination(total=db.session.scalar(count_query))
 
-    inbound_orders = db.session.scalars(
+    reports = db.session.scalars(
         query.offset((pagination.page - 1) * pagination.per_page).limit(
             pagination.per_page
         )
     )
+    return pagination, reports
 
+
+@pickup_inbound_blueprint.route("/", methods=["GET"])
+@login_required
+def get_all():
+    form_edit: f.ShipRequestForm = f.ShipRequestForm()
     return render_template(
         "pickup_inbound/pickup_inbounds.html",
-        inbound_orders=inbound_orders,
-        page=pagination,
-        search_query=q,
+        form_edit=form_edit,
         suppliers=db.session.scalars(m.Supplier.select().order_by(m.Supplier.id)),
         delivery_agents=db.session.scalars(
             m.DeliveryAgent.select().order_by(m.DeliveryAgent.id)
         ),
         warehouses=db.session.scalars(m.Warehouse.select().order_by(m.Warehouse.id)),
         products=db.session.scalars(m.Product.select().order_by(m.Product.id)),
-        form_create=form_create,
-        form_edit=form_edit,
-        form_sort=form_sort,
         inbound_orders_status=s.InboundOrderStatus,
     )
 
@@ -106,72 +112,10 @@ def pickup():
 )  # TODO move pickup inbound sort to GET with params
 @login_required
 def sort():
-    if (
-        request.method == "GET"
-        and request.args.get("page", type=str, default=None) is None
-    ):
-        flash("Sort without any arguments", "danger")
-        return redirect(url_for("pickup_inbound.get_all"))
-    form_sort: f.SortByStatusInboundOrderForm = f.SortByStatusInboundOrderForm()
-    form_create = f.InboundOrderCreateForm()
-    form_edit = f.InboundOrderUpdateForm()
-    if not form_sort.validate_on_submit() and request.method == "POST":
-        # NOTE: this is drop filters action
-        return redirect(url_for("pickup_inbound.get_all"))
-
-    filtered = True
-
-    q = request.args.get("q", type=str, default=None)
-    query = (
-        m.InboundOrder.select()
-        .where(m.InboundOrder.status == s.InboundOrderStatus(form_sort.sort_by.data))
-        .order_by(m.InboundOrder.id)
-    )
-    count_query = (
-        sa.select(sa.func.count())
-        .where(m.InboundOrder.status == s.InboundOrderStatus(form_sort.sort_by.data))
-        .select_from(m.InboundOrder)
-    )
-    if q:
-        query = query.where(
-            m.InboundOrder.title.ilike(f"%{q}%")
-            | m.InboundOrder.quantity.ilike(f"%{q}%"),
-        )
-
-        count_query = count_query.where(
-            m.InboundOrder.title.ilike(f"%{q}%")
-            | m.InboundOrder.quantity.ilike(f"%{q}%"),
-        )
-
-    pagination = create_pagination(total=db.session.scalar(count_query))
-
-    inbound_orders = db.session.scalars(
-        query.offset((pagination.page - 1) * pagination.per_page).limit(
-            pagination.per_page
-        )
-    ).all()
-    # package_info = db.session.scalars(
-    #     m.PackageInfo.select().order_by(m.PackageInfo.id)
-    # ).all()
-    # package_info_by_io = {pi.inbound_order_id: pi for pi in package_info}
+    pagination, orders = get_pickup_inbound()
 
     return render_template(
-        "pickup_inbound/pickup_inbounds.html",
-        inbound_orders=inbound_orders,
+        "pickup_inbound/pickup_inbounds_table.html",
         page=pagination,
-        search_query=q,
-        suppliers=db.session.scalars(m.Supplier.select().order_by(m.Supplier.id)).all(),
-        delivery_agents=db.session.scalars(
-            m.DeliveryAgent.select().order_by(m.DeliveryAgent.id)
-        ).all(),
-        warehouses=db.session.scalars(
-            m.Warehouse.select().order_by(m.Warehouse.id)
-        ).all(),
-        products=db.session.scalars(m.Product.select().order_by(m.Product.id)).all(),
-        # package_info_by_io=package_info_by_io,  # TODO need's refactor
-        form_create=form_create,
-        form_edit=form_edit,
-        form_sort=form_sort,
-        filtered=filtered,
-        inbound_orders_status=s.InboundOrderStatus,
+        orders=orders,
     )
