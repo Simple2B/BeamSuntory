5ff3f55f540ca9e12e0f06face5c50f9b3b95409
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2024-02-28 11:39:44 +0200

    add logs, enable email notification

--- app/controllers/image.py
+++ app/controllers/image.py
@@ -14,8 +14,10 @@ def save_image(image: BytesIO, path: str, image_model: m.Image = None):
     """Save image to disk and return the image obj."""
     kind = filetype.guess(image)
     if kind is None:
+        log(log.ERROR, "Cannot guess file type!")
         return "Cannot guess file type!"
     if not kind.mime.startswith("image"):
+        log(log.ERROR, "Unsupported file type!")
         return "Unsupported file type!"
 
     file_path = f"{path}.{kind.extension}"
@@ -27,13 +29,16 @@ def save_image(image: BytesIO, path: str, image_model: m.Image = None):
             f.write(image.read())
 
         if not image_model:
+            log(log.INFO, "Image model not provided. Creating new image model.")
             image_name = path.split("/")[-1]
             image = db.session.scalar(
                 m.Image.select().where(m.Image.name == image_name)
             )
             if image:
+                log(log.INFO, "Image already exists. Deleting old image.")
                 db.session.delete(image)
                 db.session.commit()
+                log(log.INFO, "Old image deleted.")
             return m.Image(
                 name=image_name,
                 path=image_path,
