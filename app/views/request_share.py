dffd4e27caca575e7376b6fd60c9edba322213cf
nazarkobryn <kobrinnazar05@gmail.com>
2024-10-11 15:51:22 -0400

    fix share request

--- app/views/request_share.py
+++ app/views/request_share.py
@@ -301,6 +301,11 @@ def share(id: int):
         ),
     )
 
+    request_share.status = "shared"
+    request_share.finished_date = datetime.now().replace(microsecond=0)
+    db.session.add(report_request_share)
+    db.session.commit()
+
     if request_share.user.is_notify_request_share_status:
         msg = Message(
             subject=f"Request share approved {request_share.order_numb}",
@@ -324,10 +329,6 @@ def share(id: int):
         )
         mail.send(msg)
 
-    request_share.status = "shared"
-    request_share.finished_date = datetime.now().replace(microsecond=0)
-    db.session.add(report_request_share)
-    db.session.commit()
     log(log.INFO, "Request Share share: [%s]", request_share)
     flash("Request Share shared!", "success")
     return redirect(url_for("request_share.get_all"))
